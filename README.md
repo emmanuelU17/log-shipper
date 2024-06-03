dd4e15b8af7db8b475b250a0ee6d6802c39f4b63
emmanuelU17 <emmanueluluabuike@gmail.com>
2024-06-02 22:06:55 -0400

    1st commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Simple Java application that ships application logs to discord channel
+
+## Technologies
+1. Java 21
---
+++ src/main/java/org/log/shipper/Application.java
@@ -0,0 +1,7 @@
+package org.log.shipper;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
